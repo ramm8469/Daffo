12a1b950d104749c0815d2b0599c4c150581112f
ramm8469 <ramm8469@gmail.com>
2020-02-04 15:00:43 +0530

    Worked on Call back hell in js

--- JS/Js_files/Revision.js
+++ JS/Js_files/Revision.js
@@ -96,23 +96,120 @@ var person = {
 // now let's do this in a practical manner
 
 // creating a new promises
-var p = new Promise((resolve,reject) => {
-    let a = 1+1 
-    // if we change a = 1+2, we get Failed else we get Success
-    if(a == 2){
-        resolve("Success")
-    }
-    else{
-        reject("Failed")
-    }
-}
-)
+// var p = new Promise((resolve,reject) => {
+//     let a = 1+1
+//     // if we change a = 1+2, we get Failed else we get Success
+//     if(a == 2){
+//         resolve("Success")
+//     }
+//     else{
+//         reject("Failed")
+//     }
+// }
+// )
 
 // now executing the promise
-p.then((message) => {
-    console.log("This is from then : "+message)
-})
+// p.then((message) => {
+//     console.log("This is from then : "+message)
+// })
 
-p.catch((message) => {
-    console.log("This is from catch : "+ message)
-} )
+
+
+
+
+
+// p.catch((message) => {
+//     console.log("This is from catch : "+ message)
+// } )
+
+// question 1
+
+// Promise.resolve(5)
+// .then(
+//     function(res){
+//         console.log(res)
+    
+//     setTimeout(
+//         function(){
+//             return 8
+//         }
+//         )
+//     }
+// )
+// .then(
+//     function(res){
+//         console.log(res)
+//     }
+// )
+
+
+// question 2
+// Promise.reject(5)
+//     .then(
+//         function(res){
+//             console.log(res)
+//             return 6
+//         }
+//     )
+//     .catch(
+//         function(err){
+//             console.log(err)
+//         }
+//     )
+//     .then(
+//         function(res){
+//             console.log(res)
+//             return 6
+//         }
+
+//     )
+//     .catch(
+//         function(err){
+//             console.log(err)
+//         }
+//     )
+
+
+// question 3
+// Promise.all(
+//     [
+//         Promise.resolve(5),
+//         Promise.reject(66),
+//         Promise.resolve(2)
+        
+//     ]
+// )
+// .then(console.log())
+// .catch(console.log())
+
+// Call back hell demo
+
+function foo(){
+    console.log(1)
+    setTimeout(
+        () => {
+            console.log(2),
+            setTimeout(
+                () => {
+                    console.log(3)
+                    setTimeout(
+                        () => {
+                            console.log(4)
+                        },
+                        console.log(5),
+                        1500
+                    )
+                },
+                console.log(6),
+                1500
+            )
+        },
+        console.log(7),
+        1500
+    )
+    console.log(8)
+}
+
+// calling the foo
+
+foo()
