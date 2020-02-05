43b60142dfdff7e62dcde2a819368d851d7b288a
ramm8469 <ramm8469@gmail.com>
2020-02-05 14:01:02 +0530

    Done with Map-Reduce-Filter

---
+++ JS/Js_files/Map_Reduce_Filter.js
@@ -0,0 +1,66 @@
+console.log()
+console.log("===================================================")
+console.log("Map-Reduce-Filter")
+console.log("===================================================")
+console.log()
+
+
+// Map() Demo
+
+// What you have
+var officers = [
+    { id: 20, name: 'Captain Piett' },
+    { id: 24, name: 'General Veers' },
+    { id: 56, name: 'Admiral Ozzel' },
+    { id: 88, name: 'Commander Jerjerrod' }
+  ];
+  // What you need
+  //[20, 24, 56, 88]
+
+
+
+// we can use map to get that..
+var officersIds = [];
+// officers.forEach(function (officer) {
+//     officersIds.push(officer.id);
+//   });
+officersIds = officers.map( officer => officer.id)
+
+console.log(officersIds)
+
+// Another example on map
+// square of a number
+console.log("=====Another Demo on the map() method in JS ===")
+var myNum = [1,2,3,4,5,6,7,8,9]
+var myData = myNum.map ( x => x*x)
+var mynewData = myNum.map((x,y) => (x*2+y))
+console.log(myData)
+console.log(mynewData)
+
+console.log("Question Asked On Map")
+
+// question on map
+// fibonnacci Series...
+var data = [1,2,3,4,5,6,7,8,9].map(
+    (x,y) => x+y
+)
+
+// Printing the result
+console.log(data)
+console.log("=================================")
+
+// Reduce Demo
+// i'm confused in while understanding reduce()
+
+
+// Filter Demo
+// Filter is a predefined function, used to filter out 
+// some elements from the array of elements
+
+var nums = [1,2,3,4,5,6,7,8,9,10]
+ var even = nums.filter((ele) => {
+     if(ele%2 == 0){
+         return ele
+     }
+ })
+ console.log(even)
