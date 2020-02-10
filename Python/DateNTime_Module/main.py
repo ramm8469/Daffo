b17d1e0cbe6717b2840e71ee970d69884b95eaaa
ramm8469 <ramm8469@gmail.com>
2020-02-10 18:51:17 +0530

    Learned the Time Module

---
+++ Python/DateNTime_Module/main.py
@@ -0,0 +1,14 @@
+# Here in this file we are going to see the time module available 
+# in Python
+
+#Importing the time module
+import time
+
+
+# Getting the current local time
+localtime = time.localtime(time.time())
+print ("Local current time :" ,localtime[2],"/",localtime[1],"/",localtime[0])
+print("Hours : Minutes : Seconds  === ",localtime[3],":",localtime[4],":",localtime[5])
+
+for i in range(1000000):
+    print(localtime[5])
